f3d4b10c505afc95c46ca4594a7dde8b1a8e76c9
Karolina <karolina-jesionek@wp.pl>
2024-03-08 21:34:26 +0100

    changes

--- src/components/MainPage/Navbar/Navbar.jsx
+++ src/components/MainPage/Navbar/Navbar.jsx
@@ -1,7 +1,12 @@
 import NavbarColorButton from "../Buttons/NavbarColorButton";
 import Logo from "./Logo";
 
+
+
 const Navbar = () => {
+
+
+  
   return (
     <div className="bg-transparent">
       <div className="flex justify-between py-[15px]">
