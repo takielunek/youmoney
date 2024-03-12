a6cc0a0358a87ea4efd5d69a884071ae98eace97
Karolina <karolina-jesionek@wp.pl>
2024-03-12 13:57:35 +0100

    changes

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -15,12 +15,18 @@ const Message = () => {
     <section>
       <MessageNavbar />
       <div className="px-[16px] ss:px-0 py-[32px] ss:py-[40px] sm:py-[48px] md:py-[54px] flex justify-around">
-        <div className="my-[32px] ss:my-[40px] sm:my-[48px] md:my-[54px] py-[24px] ss:py-[40px] sm:py-[48px] md:py-[54px] px-[12.8px] ss:px-[20px] sm:px-[24px] md:px-[36px] rounded-xl shadow-2xl w-full xss:w-[480px] ss:w-[510px]">
+        <div
+          className={`${theme === "light" ? "bg-white" : "bg-darkModeItemsBg"} my-[32px] ss:my-[40px] sm:my-[48px] md:my-[54px] py-[24px] ss:py-[40px] sm:py-[48px] md:py-[54px] px-[12.8px] ss:px-[20px] sm:px-[24px] md:px-[36px] rounded-xl shadow-2xl w-full xss:w-[480px] ss:w-[510px]`}
+        >
           <img src={secure} alt="Grafika" className="mb-[40px] w-[200px]" />
-          <p className=" text-[21px] sm:text-[28px] md:text-[30px]  lg:text-[31.5px] text-darkGrayish font-bold mb-[32px]">
+          <p
+            className={`${theme === "light" ? "text-darkGrayish" : "text-superLightGrey2"} text-[21px] sm:text-[28px] md:text-[30px]  lg:text-[31.5px] font-bold mb-[32px]`}
+          >
             Twój wniosek został przyjęty prawidłowo.
           </p>
-          <p className=" text-[16px] md:text-[17px] lg:text-[18px] text-grayish mb-[24px]">
+          <p
+            className={`${theme === "light" ? "text-grayish" : "text-superLightGrey2"} text-[16px] md:text-[17px] lg:text-[18px]  mb-[24px]`}
+          >
             Wkrótce otrzymasz oferty.
           </p>
         </div>
